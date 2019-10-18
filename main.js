54c4730298c715165ed5cff5f0fd99667ee3c474
fatima ahmed <fatymah001@gmail.com>
2019-10-19 01:32:32 +0300

    Implement check win and check tie for game logic

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const Player = (name,letter) => {
-	let moves = '';
+	let moves = ''; //012,345,678,036,147,258,048,246 ['012']
 	scores = 0;
-	const addMove = (position) => moves + position
-    return {letter,addMove}
+	const addMove = (position) => moves += position
+	const getName =() => name;
+	const getMoves = () => moves.length;
+	const winningMoves=['012','345','678','036','147','258','048','246'];
+	const checkIfWon = () => {
+		let check=false;
+		winningMoves.forEach((winMove)=>{
+			let count=0;
+			for(let j=0;j<moves.length;j++){
+				if(winMove.includes(moves[j])){
+					count++;
+					}
+				}
+				if(count>2){
+					check=true;
+				}
+				
+		})
+		return check;
+	} 
+    return {letter,addMove,checkIfWon,getName,getMoves}
 };
 const gameBoard = (() =>{
 	let board = ["1","2","3","4","5","6","7","8","9"];
@@ -40,6 +59,7 @@ const Game = (player_one,player_two,board) => {
 		}
 	}
 	const current_player = () => c_player 
+
 	return {
 		current_player,
 		start,
@@ -59,18 +79,24 @@ function getClickIndex(event){
 	if (game.state()){
 		if(game.board.enterMove(move,game.current_player().letter))
 		{
-			game.current_player().addMove(move)
-			game.togglePlayer();
+			game.current_player().addMove(move);
 			game.board.render();
+			if(game.current_player().checkIfWon()){
+				document.getElementById("board").innerHTML=`${game.current_player().getName()} has won!!`;
+			}
+			else if((game.player_one.getMoves()+game.player_two.getMoves())===9){
+				document.getElementById("board").innerHTML=`It's a tie!!`;
+			}
+			game.togglePlayer();
 		}
 	}
 
 }
 
 function startGame(){
-	
 	player_one_name = document.getElementById("first_player").value;
 	player_two_name = document.getElementById("second_player").value;
+	document.getElementById("players-info").innerHTML="";
 	player_one = Player(player_one_name, 'X');
 	player_two = Player(player_two_name, 'O');
 	game = Game(player_one,player_two,gameBoard);
